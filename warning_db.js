19fbe5da549e150904aaf7b0f05ac0592b25e302
V O - work.linux <valera.ovchinnikov@gmail.com>
2015-09-07 15:14:01 +0300

    warning_db

---
+++ warning_db.js
@@ -0,0 +1,9 @@
+var warnings_objects = [
+ {
+  "name": "Новый корабль",
+  "body_html": "<a href=\"http://www.citysocialclub.ru/articles/vmesto-korablya\" target=\"_blank\"><img src=\"http://static1.squarespace.com/static/55525958e4b06e6e38256a23/t/55e2df49e4b06cf8588ffbd6/1440931663568/?format=1500w\"> Обзор. Детские площадки, которые могли бы быть на месте нового «корабля».</a>",
+  "Point": {
+    "pos": "36.61343070 55.11893493"
+   }
+ }
+];
